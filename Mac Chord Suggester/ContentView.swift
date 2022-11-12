f3380fadfb8a7ab450bcfbf3c8e888c282fe9b0a
Carlos Mbendera <carlosmbendera@gmail.com>
2022-11-12 11:23:18 +0200

    Initial Commit

---
+++ Mac Chord Suggester/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Mac Chord Suggester
+//
+//  Created by Carlos Mbendera on 12/11/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Mac Chord Suggester/Mac_Chord_SuggesterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mac_Chord_SuggesterApp.swift
+//  Mac Chord Suggester
+//
+//  Created by Carlos Mbendera on 12/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct Mac_Chord_SuggesterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
